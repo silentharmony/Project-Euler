3a9f8603d1f6ac1a3062d530ff3f1bce1f14b0b4
silentharmony <melanie.kelly@mail.com>
2014-03-09 19:08:48 +0000

    Create 001.js

---
+++ 001/001.js
@@ -0,0 +1,16 @@
+var multiples = function() {
+
+    var x = 0;
+    
+    for(var i = 0; i < 1000; i++) {
+        //console.log(i);
+        if (i % 3 === 0 || i % 5 === 0) {
+            x += i;
+        }
+        
+    }
+    
+    console.log(x);
+};
+
+multiples();
